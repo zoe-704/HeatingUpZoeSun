76d16f30b8145f1bb58908982113a0c5715bd71d
zoe-704 <sunzoey@gmail.com>
2026-01-18 10:18:24 -0800

    final version

--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -9,6 +9,24 @@ import java.util.Arrays;
  */
 
 public class WeatherPatterns {
+    // Declare array to store lis at each index for memoization
+    public static int[] lis;
+
+    // DFS to find max LIS in entire temperature array
+    public static int dfs(int i, int[] temperatures) {
+        if (lis[i] != -1) return lis[i]; // return if already computed
+
+        int max_len = 1; // lis always begins with length 1
+        // DFS to find future days with higher temperatures like edges in graph
+        for (int j = i + 1; j < temperatures.length; j++) {
+            if (temperatures[j] > temperatures[i])
+                // update max length of LIS found
+                max_len = Math.max(max_len, 1 + dfs(j, temperatures));
+        }
+        // store results for memoization and return
+        lis[i] = max_len;
+        return lis[i];
+    }
 
     /**
      * Longest Warming Trend
@@ -16,7 +34,25 @@ public class WeatherPatterns {
      * @return the longest run of days with increasing temperatures
      */
     public static int longestWarmingTrend(int[] temperatures) {
-        // Initialize dp array and ints to use to find LIS
+        // initialize lis array and ints to use to find LIS
+        int n = temperatures.length, max_lis = 1;
+        lis = new int[n];
+        Arrays.fill(lis, -1); // LIS for index with value -1 has not been computed
+        // DFS from each day (node) to find LIS that begins there and return max found
+        for (int i = 0; i < n; i++) {
+            max_lis = Math.max(max_lis, dfs(i, temperatures));
+        }
+        return max_lis;
+    }
+
+    // nonrecursive lis
+    /**
+     * Longest Warming Trend
+     * @param temperatures
+     * @return the longest run of days with increasing temperatures
+     */
+    /*public static int longestWarmingTrend(int[] temperatures) {
+        // Initialize lis array and ints to use to find LIS
         int n = temperatures.length;
         int cur_lis, max_lis = 1;
         int[] lis = new int[n];
@@ -33,5 +69,5 @@ public class WeatherPatterns {
             max_lis = Math.max(max_lis, cur_lis);
         }
         return max_lis; // Return longest run
-    }
+    }*/
 }
